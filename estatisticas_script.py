8c998051b8b727147d79ba989e3f34cf9181c800
gabi-barcellos <76051807+gabi-barcellos@users.noreply.github.com>
2021-02-18 23:46:28 -0300

    Create estatisticas_script.py

---
+++ estatisticas_script.py
@@ -0,0 +1,23 @@
+def valor_maximo(lista):
+
+    return max(lista)
+
+def valor_minimo(lista):
+
+    return min(lista)
+
+def media_valores(lista):
+    soma = 0
+    for x in lista:
+        soma += int(x)
+
+    return soma / len(lista)
+
+#def razao_valores(lista):
+    #linha = 0
+    #for linha in lista:
+        #linha+=1
+        #if linha == numero_linha
+            #valor_coluna_01 = line[coluna_01]
+            #valor_coluna_02 = line[coluna_02]
+    #return
